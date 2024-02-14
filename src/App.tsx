3edd194673a4f097d4a8bf18f2c051891661b2e3
Emmanuel Kumah <mailkumah@gmail.com>
2024-02-14 09:52:01 +0000

    Fix: params need not to by typed

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,25 @@
 import { useState, useEffect } from "react";
-import "./App.css";
-import FileUploader from "./components/FileUploader";
 import ConvertApi from "convertapi-js";
+import "./App.css";
+
+//authorization
+let convertApi = ConvertApi.auth(import.meta.env.VITE_API_KEY);
+let params = convertApi.createParams();
 
 function App() {
-  const [selectedFile, setSelectedFile] = useState<File | string[]>();
+  const [hasfile, sethasFile] = useState<boolean>(false);
   const [status, setStatus] = useState<
     "initial" | "uploading" | "success" | "fail"
   >("initial");
 
-  // useEffect(() => {
-  //   convertFile();
-  // }, [selectedFile]);
-
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files && event.target.files.length > 0) {
-      const file = event.target.files[0];
-      convertFile(file);
-      // setSelectedFile(file);
-      //setStatus("success");
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      params.add("File", e.target.files[0]);
+      convertFile();
     }
   };
 
-  const convertFile = async (file: File) => {
-    let convertApi = ConvertApi.auth(import.meta.env.VITE_API_KEY);
-
-    let params = convertApi.createParams();
-    params.add("file", file);
+  const convertFile = async () => {
     try {
       let result = await convertApi.convert("docx", "pdf", params);
       console.log(result.files[0].Url);
@@ -35,13 +28,20 @@ function App() {
     }
   };
 
-  // // Get result file URL
-  // let url = result.files[0].Url;
+ 
 
   return (
     <>
       <h2>File Conversion made easy</h2>
-      <FileUploader onChange={handleFileChange} />
+      <div>
+        <label htmlFor="file">Choose a file </label>
+        <input
+          type="file"
+          id="file"
+          accept="application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+          onChange={handleFileChange}
+        />
+      </div>
       <p>{status}</p>
     </>
   );
--- src/components/FileUploader.tsx
+++ src/components/FileUploader.tsx
@@ -1,15 +1,18 @@
-
 type TFileProps = {
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
 const FileUploader: React.FC<TFileProps> = ({ onChange }) => {
-  
-
   return (
     <div>
       <label htmlFor="file">Choose a file </label>
-      <input type="file" name="" id="file" onChange={onChange} />
+      <input
+        type="file"
+        name=""
+        id="file"
+        accept="application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+        onChange={onChange}
+      />
     </div>
   );
 };
